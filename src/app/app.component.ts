e6d0934259c2a4adbcaa42f12301ae647da27da5
Billy <tb.mokolokoto@gmail.com>
2019-06-12 11:35:16 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { animationFrameScheduler } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'exampleAPP';
+  animal = ["dog","cat"];
+  newAnimal:string='';
+  removeAnimal:string='';
+  list = [];
+  newList :string='';
+
+  addNew(){
+    this.animal.push(this.newAnimal)
+
+  }
+
+  removeNew(list) {
+let index = list.indexOf(list)   
+      this.animal.splice(index,1) 
+
+  } 
+
+
+
+  addList(){
+    let index = 
+    this.list.push(this.newList)
+
+  }
+
+  delete(display) {
+    let index = display.indexOf(display)   
+          this.list.splice(index,1) 
+    
+      }
 }
